38717d34010ffda240a0edb26c9b1b4395436413
Aditya Bawankule <adityabawankule@gmail.com>
2017-04-11 16:31:12 -0500

    Fixed comments

--- Button.ino
+++ Button.ino
@@ -8,7 +8,7 @@
  The circuit:
  * LED attached from pin 13 to ground
  * pushbutton attached to pin 2 from +5V
- * 10K resistor attached to pin 2 from ground
+ 
 
  * Note: on most Arduinos there is already an LED on the board
  attached to pin 13.
